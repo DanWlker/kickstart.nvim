b3c0bbaa16a2a84e1e1b52e159f5529179b13a0a
DanWlker <danielhee2@gmail.com>
2024-05-11 00:28:15 +0800

    change stuff around

--- lua/custom/plugins/trouble.lua
+++ lua/custom/plugins/trouble.lua
@@ -1,33 +1,36 @@
 return {
   'folke/trouble.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  branch = 'dev', --IMPORTANT!
   config = function()
-    require('trouble').setup {
+    local trouble = require 'trouble'
+
+    trouble.setup {
       icons = false,
       fold_open = 'v', -- icon used for open folds
       fold_closed = '>', -- icon used for closed folds
       indent_lines = false, -- add an indent guide below the fold icons
       signs = {
         -- icons / text used for a diagnostic
-        error = 'error',
-        warning = 'warn',
-        hint = 'hint',
-        information = 'info',
+        error = '[X]',
+        warning = '<!>',
+        hint = '{?}',
+        information = '(i)',
       },
       use_diagnostic_signs = false, -- enabling this will use the signs defined in your lsp client
     }
+
     -- Lua
     vim.keymap.set('n', '<leader>xx', function()
-      require('trouble').toggle()
+      trouble.toggle()
     end)
     vim.keymap.set('n', '<leader>xw', function()
-      require('trouble').toggle 'workspace_diagnostics'
+      trouble.toggle 'workspace_diagnostics'
     end)
     vim.keymap.set('n', '<leader>xd', function()
-      require('trouble').toggle 'document_diagnostics'
+      trouble.toggle 'document_diagnostics'
     end)
     vim.keymap.set('n', 'gR', function()
-      require('trouble').toggle 'lsp_references'
+      trouble.toggle 'lsp_references'
     end)
   end,
 }
