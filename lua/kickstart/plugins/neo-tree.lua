898020594c160903fe1f36fea1adca50c3ffb958
DanWlker <danielhee2@gmail.com>
2024-05-12 02:38:44 +0800

    Customize neo-tree but won't use it as need to learn netrw

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -6,20 +6,54 @@ return {
   version = '*',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
   },
   cmd = 'Neotree',
   keys = {
     { '\\', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
   },
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
+  config = function()
+    require('neo-tree').setup {
+      default_component_configs = {
+        icon = {
+          folder_closed = '>',
+          folder_open = 'v',
+          folder_empty = '-',
+          -- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
+          -- then these will never be used.
+          default = ' ',
+          highlight = 'NeoTreeFileIcon',
+        },
+        git_status = {
+          symbols = {
+            -- Change type
+            added = '+', -- or "✚", but this is redundant info if you use git_status_colors on the name
+            modified = '~', -- or "", but this is redundant info if you use git_status_colors on the name
+            deleted = 'x', -- this can only be used in the git_status source
+            renamed = 'r', -- this can only be used in the git_status source
+            -- Status type
+            untracked = '?',
+            ignored = '/',
+            unstaged = 'U',
+            staged = '*',
+            conflict = '!',
+          },
         },
       },
-    },
-  },
+      filesystem = {
+        window = {
+          position = 'right',
+          mappings = {
+            ['\\'] = 'close_window',
+          },
+        },
+        follow_current_file = {
+          enabled = true,
+        },
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        hijack_netrw_behavior = 'open_current',
+      },
+    }
+  end,
 }
