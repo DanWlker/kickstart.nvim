52ec8b335fe9eb18fd05056a4dfd8c6da5564b47
DanWlker <danielhee2@gmail.com>
2024-05-13 00:31:11 +0800

    Add custom keybinding

--- init.lua
+++ init.lua
@@ -76,6 +76,11 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Custom Keybinding
+vim.keymap.set('n', 'Q', '<nop>')
+vim.keymap.set('v', '<S-Up>', ":m '<-2<CR>gv=gv")
+vim.keymap.set('v', '<S-Down>', ":m '>+1<CR>gv=gv")
+
 --  See `:help lua-guide-autocommands`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
