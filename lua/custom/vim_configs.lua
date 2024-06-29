fa1c770d941af823300650b757c9525ec2da1dd9
DanWlker <danielhee2@gmail.com>
2024-06-30 00:15:22 +0800

    Add experimental lines

--- lua/custom/vim_configs.lua
+++ lua/custom/vim_configs.lua
@@ -7,3 +7,10 @@ vim.opt.colorcolumn = '80'
 vim.keymap.set('n', 'Q', '<nop>')
 vim.keymap.set('v', '<S-Up>', ":m '<-2<CR>gv=gv")
 vim.keymap.set('v', '<S-Down>', ":m '>+1<CR>gv=gv")
+
+-- Change cursor color based on vim mode
+-- vim.cmd 'hi ModesNormal guibg=#f4dbd6'
+-- vim.cmd 'hi ModesInsert guibg=#a6da95'
+-- vim.cmd 'hi ModesVisual guibg=#c6a0f6'
+-- vim.cmd 'hi ModesReplace guibg=#ed8796'
+-- vim.opt.guicursor = 'n-o:block-ModesNormal,i:block-ModesInsert,v-ve:block-ModesVisual,r:block-ModesReplace,c-ci-cr-sm:block-lCursor'
