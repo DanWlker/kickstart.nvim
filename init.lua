8a94c4da97127c4545240265a2f6795726ac873b
DanWlker <danielhee2@gmail.com>
2024-07-01 01:38:33 +0800

    Remove redundant things and customize mini statusline

--- init.lua
+++ init.lua
@@ -681,6 +681,33 @@ require('lazy').setup({
         return '%2l:%-2v'
       end
 
+      local CTRL_S = vim.api.nvim_replace_termcodes('<C-S>', true, true, true)
+      local CTRL_V = vim.api.nvim_replace_termcodes('<C-V>', true, true, true)
+      statusline.section_mode = function(args)
+        local modes = setmetatable({
+          ['n'] = { long = '(˵•̀ ᴗ -)', short = 'N', hl = 'MiniStatuslineModeNormal' },
+          ['v'] = { long = '( -_・)σ', short = 'V', hl = 'MiniStatuslineModeVisual' },
+          ['V'] = { long = '( -_・)σ', short = 'V-L', hl = 'MiniStatuslineModeVisual' },
+          [CTRL_V] = { long = '( -_・)σ', short = 'V-B', hl = 'MiniStatuslineModeVisual' },
+          ['s'] = { long = '(´ ▽｀) ', short = 'S', hl = 'MiniStatuslineModeVisual' },
+          ['S'] = { long = '(´ ▽｀) ', short = 'S-L', hl = 'MiniStatuslineModeVisual' },
+          [CTRL_S] = { long = '(´ ▽｀) ', short = 'S-B', hl = 'MiniStatuslineModeVisual' },
+          ['i'] = { long = '(•̀ - •́ )', short = 'I', hl = 'MiniStatuslineModeInsert' },
+          ['R'] = { long = '( •̯́ ₃ •̯̀)', short = 'R', hl = 'MiniStatuslineModeReplace' },
+          ['c'] = { long = 'Σ(°△°ꪱꪱ)', short = 'C', hl = 'MiniStatuslineModeCommand' },
+          ['r'] = { long = 'Σ(°△°ꪱꪱ)', short = 'P', hl = 'MiniStatuslineModeOther' },
+          ['!'] = { long = 'Σ(°△°ꪱꪱ)', short = 'Sh', hl = 'MiniStatuslineModeOther' },
+          ['t'] = { long = ' (⌐■_■) ', short = 'T', hl = 'MiniStatuslineModeOther' },
+        }, {
+          __index = function()
+            return { long = 'Unknown', short = 'U', hl = '%#MiniStatuslineModeOther#' }
+          end,
+        })
+        local mode_info = modes[vim.fn.mode()]
+        local mode = MiniStatusline.is_truncated(args.trunc_width) and mode_info.short or mode_info.long
+        return mode, mode_info.hl
+      end
+
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
