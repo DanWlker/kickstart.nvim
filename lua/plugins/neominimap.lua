2535c9e260deef2ef80e78906ec7667e0fd43dd4
DanWlker <danielhee2@gmail.com>
2024-10-14 21:55:11 +0800

    stuff

--- lua/plugins/neominimap.lua
+++ lua/plugins/neominimap.lua
@@ -20,6 +20,34 @@ return {
         z_index = 21, -- must be higher than treesitter context
         window_border = 'rounded',
       },
+      click = {
+        enabled = true,
+      },
+      winopt = function(opt, winid)
+        return {
+          winhighlight = table.concat({
+            'Normal:NeominimapBackground',
+            'FloatBorder:NeominimapBorder',
+            'CursorLine:NeominimapCursorLine',
+            'CursorLineNr:NeominimapCursorLineNr',
+            'CursorLineSign:NeominimapCursorLineSign',
+            'CursorLineFold:NeominimapCursorLineFold',
+          }, ','),
+          wrap = false,
+          foldcolumn = '0',
+          signcolumn = 'auto',
+          number = false,
+          relativenumber = false,
+          scrolloff = 99999, -- To center minimap
+          sidescrolloff = 0,
+          winblend = 0,
+          cursorline = true,
+          spell = false,
+          list = false,
+          fillchars = 'eob: ',
+          winfixwidth = true,
+        }
+      end,
     }
   end,
 }
