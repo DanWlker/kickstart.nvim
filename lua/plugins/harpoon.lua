5cf109a3c4dd47ef161ef5b83cb86799b30761d4
DanWlker <danielhee2@gmail.com>
2024-10-22 21:15:19 +0800

    update harpoon modes

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -8,7 +8,7 @@ return {
       function()
         require('harpoon'):list():add()
       end,
-      mode = { 'n', 'v' },
+      mode = 'n',
       desc = '[H]arpoon [A]dd',
     },
     {
@@ -16,7 +16,7 @@ return {
       function()
         require('harpoon').ui:toggle_quick_menu(require('harpoon'):list())
       end,
-      mode = { 'n', 'v' },
+      mode = 'n',
       desc = '[H]arpoon [E]dit',
     },
     {
@@ -24,28 +24,28 @@ return {
       function()
         require('harpoon'):list():select(1)
       end,
-      mode = { 'n', 'v' },
+      mode = 'n',
     },
     {
       '<C-a>',
       function()
         require('harpoon'):list():select(2)
       end,
-      mode = { 'n', 'v' },
+      mode = 'n',
     },
     {
       '<C-e>',
       function()
         require('harpoon'):list():select(3)
       end,
-      mode = { 'n', 'v' },
+      mode = 'n',
     },
     {
       '<C-i>',
       function()
         require('harpoon'):list():select(4)
       end,
-      mode = { 'n', 'v' },
+      mode = 'n',
     },
   },
   config = function()
