25667097cdc3c00fb56b082d7c4985871e3a65c2
DanWlker <danielhee2@gmail.com>
2024-10-24 00:03:21 +0800

    update keymap and bufferline design

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -16,18 +16,18 @@ return {
         end,
       },
       highlights = require('catppuccin.groups.integrations.bufferline').get {
-        styles = { 'italic', 'bold' },
-        custom = {
-          all = {
-            fill = { bg = mocha.base },
-          },
-          mocha = {
-            background = { fg = mocha.text },
-          },
-          latte = {
-            background = { fg = '#000000' },
-          },
-        },
+        -- styles = { 'italic', 'bold' },
+        -- custom = {
+        --   all = {
+        --     fill = { bg = mocha.base },
+        --   },
+        --   mocha = {
+        --     background = { fg = mocha.text },
+        --   },
+        --   latte = {
+        --     background = { fg = '#000000' },
+        --   },
+        -- },
       },
     }
   end,
