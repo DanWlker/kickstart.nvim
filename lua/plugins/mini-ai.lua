c774390bc2090128638583d255bd416fab0a60d5
DanWlker <danielhee2@gmail.com>
2024-11-21 23:15:06 +0800

    update mini-ai keybind

--- lua/plugins/mini-ai.lua
+++ lua/plugins/mini-ai.lua
@@ -1,4 +1,62 @@
 -- Everything in here is from LazyVim
+local function ai_whichkey(opts)
+  local objects = {
+    { ' ', desc = 'whitespace' },
+    { '"', desc = '" string' },
+    { "'", desc = "' string" },
+    { '(', desc = '() block' },
+    { ')', desc = '() block with ws' },
+    { '<', desc = '<> block' },
+    { '>', desc = '<> block with ws' },
+    { '?', desc = 'user prompt' },
+    { 'U', desc = 'use/call without dot' },
+    { '[', desc = '[] block' },
+    { ']', desc = '[] block with ws' },
+    { '_', desc = 'underscore' },
+    { '`', desc = '` string' },
+    { 'a', desc = 'argument' },
+    { 'b', desc = ')]} block' },
+    { 'c', desc = 'class' },
+    { 'd', desc = 'digit(s)' },
+    { 'e', desc = 'CamelCase / snake_case' },
+    { 'f', desc = 'function' },
+    { 'g', desc = 'entire file' },
+    { 'i', desc = 'indent' },
+    { 'o', desc = 'block, conditional, loop' },
+    { 'q', desc = 'quote `"\'' },
+    { 't', desc = 'tag' },
+    { 'u', desc = 'use/call' },
+    { '{', desc = '{} block' },
+    { '}', desc = '{} with ws' },
+  }
+
+  local ret = { mode = { 'o', 'x' } }
+  ---@type table<string, string>
+  local mappings = vim.tbl_extend('force', {}, {
+    around = 'a',
+    inside = 'i',
+    around_next = 'an',
+    inside_next = 'in',
+    around_last = 'al',
+    inside_last = 'il',
+  }, opts.mappings or {})
+  mappings.goto_left = nil
+  mappings.goto_right = nil
+
+  for name, prefix in pairs(mappings) do
+    name = name:gsub('^around_', ''):gsub('^inside_', '')
+    ret[#ret + 1] = { prefix, group = name }
+    for _, obj in ipairs(objects) do
+      local desc = obj.desc
+      if prefix:sub(1, 1) == 'i' then
+        desc = desc:gsub(' with ws', '')
+      end
+      ret[#ret + 1] = { prefix .. obj[1], desc = obj.desc }
+    end
+  end
+  require('which-key').add(ret, { notify = false })
+end
+
 local function ai_indent(ai_type)
   local spaces = (' '):rep(vim.o.tabstop)
   local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
@@ -54,9 +112,9 @@ return {
   'echasnovski/mini.ai',
   event = 'VimEnter',
   dependencies = 'nvim-treesitter/nvim-treesitter-textobjects',
-  config = function()
+  opts = function()
     local ai = require 'mini.ai'
-    ai.setup {
+    return {
       n_lines = 500,
       custom_textobjects = {
         o = ai.gen_spec.treesitter { -- code block
@@ -77,5 +135,9 @@ return {
         U = ai.gen_spec.function_call { name_pattern = '[%w_]' }, -- without dot in function name
       },
     }
+  end,
+  config = function(_, opts)
+    require('mini.ai').setup(opts)
+    ai_whichkey(opts)
   end,
 }
