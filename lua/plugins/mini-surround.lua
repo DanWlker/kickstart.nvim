1aa39285eddeb91b423d77ad626ee58cff5758e9
DanWlker <danielhee2@gmail.com>
2024-11-22 23:57:37 +0800

    disable certain surround keybinds

--- lua/plugins/mini-surround.lua
+++ lua/plugins/mini-surround.lua
@@ -20,13 +20,13 @@ return {
   end,
   opts = {
     mappings = {
-      add = 'sa', -- Add surrounding in Normal and Visual modes
+      add = 's', -- Add surrounding in Normal and Visual modes
       delete = 'sd', -- Delete surrounding
-      find = 'sf', -- Find surrounding (to the right)
-      find_left = 'sF', -- Find surrounding (to the left)
-      highlight = 'sh', -- Highlight surrounding
+      find = '', -- Find surrounding (to the right)
+      find_left = '', -- Find surrounding (to the left)
+      highlight = '', -- Highlight surrounding
       replace = 'sr', -- Replace surrounding
-      update_n_lines = 'sn', -- Update `n_lines`
+      update_n_lines = '', -- Update `n_lines`
     },
   },
 }
