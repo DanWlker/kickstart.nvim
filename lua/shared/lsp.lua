4959d6c713eec34a6462604696f1c6fc2c7d725e
DanWlker <danielhee2@gmail.com>
2024-11-23 10:30:26 +0800

    copy lsp configs from lazyvim

--- lua/shared/lsp.lua
+++ lua/shared/lsp.lua
@@ -7,12 +7,43 @@ M.getServers = function()
       settings = {
         gopls = {
           gofumpt = true,
+          codelenses = {
+            gc_details = false,
+            generate = true,
+            regenerate_cgo = true,
+            run_govulncheck = true,
+            test = true,
+            tidy = true,
+            upgrade_dependency = true,
+            vendor = true,
+          },
+          hints = {
+            assignVariableTypes = true,
+            compositeLiteralFields = true,
+            compositeLiteralTypes = true,
+            constantValues = true,
+            functionTypeParameters = true,
+            parameterNames = true,
+            rangeVariableTypes = true,
+          },
+          analyses = {
+            fieldalignment = true,
+            nilness = true,
+            unusedparams = true,
+            unusedwrite = true,
+            useany = true,
+          },
+          usePlaceholders = true,
+          completeUnimported = true,
+          staticcheck = true,
+          directoryFilters = { '-.git', '-.vscode', '-.idea', '-.vscode-test', '-node_modules' },
+          semanticTokens = true,
         },
       },
     },
     gofumpt = {},
-    -- pyright = {},
-    -- rust_analyzer = {},
+
+    -- Vue, but its messy, probably should refer to LazyVim config
     ts_ls = {
       init_options = {
         plugins = {
@@ -33,9 +64,25 @@ M.getServers = function()
     html = {},
     cssls = {},
     volar = {},
+
+    -- Lua
     lua_ls = {
       settings = {
         Lua = {
+          codeLens = {
+            enable = true,
+          },
+          doc = {
+            privateName = { '^_' },
+          },
+          hint = {
+            enable = true,
+            setType = false,
+            paramType = true,
+            paramName = 'Disable',
+            semicolon = 'Disable',
+            arrayIndex = 'Disable',
+          },
           completion = {
             callSnippet = 'Replace',
           },
