bf617f2416ec47224f43b5c6de6ef501fe9ce00c
DanWlker <danielhee2@gmail.com>
2025-07-16 21:16:20 +0800

    Revert "remove test cases"
    This reverts commit a00e462c244e0585e296f1cc8d11fe9d3d69d017.

--- init.lua
+++ init.lua
@@ -22,6 +22,93 @@ require('lazy').setup({
 
   -- [[ Plugins ]]
   { import = 'plugins' },
+
+  -- {
+  --   dir = '~/projects/toolbox.nvim',
+  --   name = 'toolbox',
+  --   config = function()
+  --     require('toolbox').setup {
+  --       commands = {
+  --         {
+  --           -- name = 'Format Json Req Input',
+  --           name = 'A',
+  --           execute = "%!jq '.'<Esc>",
+  --           require_input = true,
+  --           weight = 0,
+  --         },
+  --         {
+  --           -- name = 'format leader',
+  --           name = 'c',
+  --           execute = 's/leader/thing',
+  --           weight = -1,
+  --         },
+  --         {
+  --           -- name = 'format leader req input',
+  --           name = 'b',
+  --           execute = 's/leader/thing',
+  --           require_input = true,
+  --         },
+  --         {
+  --           -- name = 'Format Json',
+  --           name = 'F',
+  --           execute = "%!jq '.'",
+  --         },
+  --         {
+  --           name = 'variadic',
+  --           execute = function(...)
+  --             local args = { ... }
+  --             print(vim.inspect(args))
+  --           end,
+  --           require_input = true,
+  --         },
+  --         {
+  --           name = 'normal',
+  --           execute = function()
+  --             print 'this is normal'
+  --           end,
+  --           require_input = true,
+  --         },
+  --         {
+  --           -- name = 'Print My NAME',
+  --           name = 'this1',
+  --           execute = function(item)
+  --             print('The name is ' .. item .. '-agen')
+  --           end,
+  --           tags = { 'second' },
+  --         },
+  --         {
+  --           -- name = 'Print My NAME<Left><Left><Left>',
+  --           name = 'this2',
+  --           execute = function(item, item2, item3)
+  --             print('The name is ' .. item .. '-agen')
+  --           end,
+  --           tags = { 'first', 'second' },
+  --         },
+  --       },
+  --     }
+  --
+  --     vim.keymap.set({ 'n', 'v' }, '<leader>w', function()
+  --       vim.list_extend(require('toolbox').opts.commands, {
+  --         {
+  --           -- name = 'Print My NAME<Left><Left><Left>',
+  --           name = 'this4',
+  --           execute = function(item, item2, item3)
+  --             print('The name is ' .. item .. '-agen')
+  --           end,
+  --           tags = { 'first', 'second' },
+  --         },
+  --       })
+  --     end, { desc = '' })
+  --
+  --     vim.keymap.set({ 'n', 'v' }, '<leader>s', function()
+  --       require('toolbox').show_picker(nil, {
+  --         layout = {
+  --           preset = 'telescope',
+  --         },
+  --       })
+  --     end, { desc = '' })
+  --   end,
+  -- },
 }, {
   ui = {
     icons = vim.g.have_nerd_font and {} or {
