78b49f881ba7f8fb9b1687655875f1ed4fe7e09d
DanWlker <danielhee2@gmail.com>
2025-08-25 22:21:26 +0800

    add import nvim

---
+++ lua/plugins/import.lua
@@ -0,0 +1,23 @@
+-- Lazy
+return {
+  'piersolenski/import.nvim',
+  dependencies = {
+    -- One of the following pickers is required:
+    'nvim-telescope/telescope.nvim',
+    -- 'folke/snacks.nvim',
+    -- 'ibhagwan/fzf-lua',
+  },
+  opts = {
+    picker = 'telescope',
+    insert_at_top = false,
+  },
+  keys = {
+    {
+      '<leader>fi',
+      function()
+        require('import').pick()
+      end,
+      desc = 'Import',
+    },
+  },
+}
