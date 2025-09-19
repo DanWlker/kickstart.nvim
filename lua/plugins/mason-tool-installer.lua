af3b21a66815d1741684811279cc7e7a06adf0ce
Hee ZiXi <zixi.hee@paynet.my>
2025-09-19 14:48:58 +0800

    don't automatically enable, only use nvim lspconfig

--- lua/plugins/mason-tool-installer.lua
+++ lua/plugins/mason-tool-installer.lua
@@ -2,7 +2,11 @@ return {
   'WhoIsSethDaniel/mason-tool-installer.nvim',
   opts = { ensure_installed = require('shared.tools').ensureInstalled },
   dependencies = {
-    { 'mason-org/mason-lspconfig.nvim', dependencies = { 'mason-org/mason.nvim' } },
+    {
+      'mason-org/mason-lspconfig.nvim',
+      dependencies = { 'mason-org/mason.nvim' },
+      opts = { automatic_enable = false },
+    },
     { 'jay-babu/mason-nvim-dap.nvim', dependencies = { 'mason-org/mason.nvim' } },
     'mason-org/mason.nvim',
   },
