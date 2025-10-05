483a082460d81742ad6558e0364c6a1852be2992
DanWlker <danielhee2@gmail.com>
2025-10-05 19:43:09 +0800

    update stuff

--- lua/personal/winbar.lua
+++ lua/personal/winbar.lua
@@ -15,7 +15,7 @@ function M.render()
   end
 
   -- Replace slashes by arrows.
-  local separator = ' %#WinbarSeparator# '
+  local separator = ' %#WinbarSeparator# '
 
   local prefix, prefix_path = '', ''
 
